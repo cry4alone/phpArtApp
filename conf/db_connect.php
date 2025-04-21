ca22753d11dbf8334d84d93db7de1dd1eff12cab
Andrei <chernenkoa04@gmail.com>
2025-04-21 19:38:36 +0700

    singleton pattern db

--- conf/db_connect.php
+++ conf/db_connect.php
@@ -1,8 +1,17 @@
 <?php
 class DB{
+    private static $pdo = null;
+    private function __construct() {}
 
 	public static function connectToDB() {
-		static $connection = pg_connect("host=db port=5432 dbname=mydb user=root password=root");
-		return $connection;
+        $dsn = "pgsql:host=db;port=5432;dbname=mydb;user=root;password=root";
+        if(self::$pdo === null) {
+            try {
+                self::$pdo = new PDO($dsn);
+            } catch (PDOException $e) {
+                echo "Ошибка подключения: " . $e->getMessage();
+            }
+        }
+		return self::$pdo;
 	}
 }
