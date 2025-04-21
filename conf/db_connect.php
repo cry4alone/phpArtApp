11ccd8e2a23f24a201909fd86c4630d3df0fd9a5
MiFit1 <MiFit1@yandex.ru>
2025-04-21 20:52:48 +0700

    add filling of database

--- conf/db_connect.php
+++ conf/db_connect.php
@@ -1,8 +1,37 @@
 <?php
 class DB{
 
+	//Заполнение БД тестовыми данными
+	public static function fillDB(){
+		$connection = self::connectToDB();
+
+		//Наличие таблицы User
+		$result = pg_query($connection, "SELECT to_regclass('public.\"User\"');");
+        $tableExists = pg_fetch_result($result, 0, 0) !== null;
+
+		if (!$tableExists) {
+            $createTableQuery = "
+                CREATE TABLE \"User\" (
+                    id SERIAL PRIMARY KEY,
+					email VARCHAR(100) NOT NULL,
+                    login VARCHAR(100) NOT NULL,
+					password VARCHAR(100) NOT NULL
+                );
+            ";
+            pg_query($connection, $createTableQuery);
+
+            $insertDataQuery = "
+                INSERT INTO \"User\" (email, login, password) 
+                VALUES 
+                    ('andrey@example.com', 'andrey', 'qwerty'),
+                    ('admin@example.com', 'admin', 'admin');
+            ";
+            pg_query($connection, $insertDataQuery);
+        }
+	}
+
 	public static function connectToDB() {
-		static $connection = pg_connect("host=db port=5432 dbname=mydb user=root password=root");
+		$connection = pg_connect("host=db port=5432 dbname=mydb user=root password=root");
 		return $connection;
 	}
 }
