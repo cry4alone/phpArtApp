678872ea6d25ff517c8d2d4cbbd6f9dda5527549
MiFit1 <MiFit1@yandex.ru>
2025-04-28 19:18:20 +0700

    Added a change in access rights to the uploads folder

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ ! -d /var/www/html/public/images/uploads ]; then
+    mkdir -p /var/www/html/public/images/uploads
+fi
+
+chown -R www-data:www-data /var/www/html/public/images/uploads
+chmod -R 775 /var/www/html/public/images/uploads
+
+composer install
+php /var/www/html/app/conf/init_db.php
+a2enmod rewrite
+
+exec apache2-foreground
