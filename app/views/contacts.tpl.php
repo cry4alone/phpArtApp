61d43957a63c56a11b020f88697e0adfe50907e5
MiFit1 <MiFit1@yandex.ru>
2025-05-04 14:24:00 +0700

    Added contacts page

--- app/views/contacts.tpl.php
+++ app/views/contacts.tpl.php
@@ -1,3 +1,33 @@
 <body>
-    Contacts page!
+  <div class="container py-5">
+    <h1 class="display-3 text-center my-5">Команда Pixels</h1>
+
+    <div class="row align-items-center justify-content-center mb-5">
+      <div class="col-12 col-md-3 text-center text-md-end">
+        <img src="/public/images/assets/developer_1.jpg" class="img-fluid rounded" width="250" height="250" alt="ГигаЧад Байтсон">
+      </div>
+      <div class="col-12 col-md-5 mt-3 mt-md-0">
+        <h2 class="mb-3">Черненко <span class="text-primary">Андрей</span></h2>
+        <p>Team Lead проекта, а также ловец багов, который чинит одно и ломает два.</p>
+        <p>Email: <a href="andreychernenko@pixels.fun">andreychernenko@pixels.fun</a></p>
+      </div>
+    </div>
+
+    <div class="row align-items-center justify-content-center mb-5">
+      <div class="col-12 col-md-3 text-center text-md-end">
+        <img src="/public/images/assets/developer_2.jpg" class="img-fluid rounded" width="250" height="250" alt="Флоппи Дисксон">
+      </div>
+      <div class="col-12 col-md-5 mt-3 mt-md-0">
+        <h2 class="mb-3">Елисеев <span class="text-primary">Василий</span></h2>
+        <p>Junior Developer, инженер костылестроения с опытом.</p>
+        <p>Email: <a href="mailto:vasiliyeliseev@pixels.fun">vasiliyeliseev@pixels.fun</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-primary-subtle d-flex justify-content-center align-items-center py-5">
+    <h1 class="h3 text-center">
+      Мы не идеальны, но зато отвечаем на письма (иногда).<br> Обращайтесь к нам!
+    </h1>
+  </div>
 </body>
